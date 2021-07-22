997f12001772896e591cc82ab4b03a32ff068081
Dhanasekaran Ravichandran <76101113+dhanar98@users.noreply.github.com>
2021-07-22 14:29:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="bootstrap-icons">
         <div className="text-center">
           <a href="https://www.linkedin.com/in/dhanar98/">
-            <i class="bi bi-linkedin"></i>linkedin
+            <i className="bi bi-linkedin"></i>linkedin
           </a>
           <a href="https://github.com/dhanar98">
             <i className="bi bi-github"></i>Github
